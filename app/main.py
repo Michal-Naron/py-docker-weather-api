cd99581ec24ee68fdb9afe6682c7dc617a1ae8f3
Michal-Naron <michanaron@gmail.com>
2025-10-05 20:28:42 +0200

    add task solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+from dotenv import load_dotenv
 
+def get_weather() -> None:
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    params = {"key": API_KEY, "q": "Paris"}
+    response = requests.get("https://api.weatherapi.com/v1/current.json", params=params)
+    data = response.json()
+    location = data["location"]
+    current = data["current"]
+    condition = current["condition"]
+    message = (
+        f"{location['name']}/{location['country']} "
+        f"{location['localtime']} "
+        f"Weather: {current['temp_c']}°C, {condition['text']}"
+    )
+    print(message)
 
 if __name__ == "__main__":
     get_weather()
